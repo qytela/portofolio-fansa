e942de096a9eff78a63f90af9ace51d64223aae9
Qytela <julfanshahidayah@gmail.com>
2021-02-01 14:54:58 +0700

    update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: false,
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
