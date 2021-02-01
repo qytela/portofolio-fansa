e1906e863f8912704f3184d342896f3efec18521
Qytela <julfanshahidayah@gmail.com>
2021-02-01 18:24:27 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,61 @@
-import React, { Fragment, memo } from 'react'
-import { FaGithub, FaDribbble, FaTwitter, FaRegEnvelope } from 'react-icons/fa'
-import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
+import React, { Fragment, memo, useState } from 'react'
+import { FaGithub, FaLinkedinIn, FaTelegramPlane, FaRegEnvelope } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
 const Home = () => {
+    const portofolios = [
+        {
+            name: 'Dilaundri',
+            description: 'Aplikasi dilaundri bisa menjadi opsi atas pakaian kamu yang menumpuk tapi kamu gak punya waktu untuk mencucinya, hal itu tidak menjadi alasan untuk kamu semakin pusing tujuh keliling dalam menjalani hari. Tidak bisa dipungkiri bahwa perkembangan teknologi semakin mumpuni di masa sekarang, khususnya di bidang laundry pakaian. Perkembangan tersebut menciptakan inovasi yang cocok banget untuk kamu yang males gerak alias mager dalam mencuci pakaian sendiri atau bahkan mencari jasa laundry secara langsung. Perkembangan teknologi dan inovasi pastinya diikuti dengan kelebihan dan keuntungan yang menyertainya.',
+            icon: '/dilaundri-icon.png',
+            url: 'https://play.google.com/store/apps/details?id=com.dilaundri'
+        },
+        {
+            name: 'BNI Lelang Agunan',
+            description: 'Dengan menggunakan aplikasi ini anda dapat mendapatkan informasi agunan baik rumah, tanah dll yang status penjualannya di lelang, baik dari searching location maupun kategori.',
+            icon: '/bni-icon.png',
+            url: 'https://play.google.com/store/apps/details?id=com.newbnilelang'
+        },
+        {
+            name: 'KlikCoaching',
+            description: 'Dengan menggunakan aplikasi ini anda dapat mendapatkan informasi agunan baik rumah, tanah dll yang status penjualannya di lelang, baik dari searching location maupun kategori.',
+            icon: '/klikcoaching-icon.png',
+            url: 'https://klikcoaching.com'
+        },
+        {
+            name: 'Komunkasi Langit',
+            description: 'Dengan menggunakan aplikasi ini anda dapat mendapatkan informasi agunan baik rumah, tanah dll yang status penjualannya di lelang, baik dari searching location maupun kategori.',
+            icon: '/komulang-icon.png',
+            url: 'http://komunikasilangit.com'
+        },
+        {
+            name: 'KumpulModal',
+            description: 'Kumpulmodal adalah aplikasi penggalangan modal usaha bagi pedagang kaki lima, yang menghubungkan Anda sebagai Investor dengan mendanai usaha pedagang kaki lima di Indonesia. Dengan Kumpulmodal, Anda telah membuat dampak sosial dan mendapatkan penghasilan.',
+            icon: '/kumpulmodal-icon.png',
+            url: 'https://play.google.com/store/apps/details?id=com.kumpulmodal'
+        }
+    ]
+
     const renderPortofolios = (
-        Array.from(Array(4), (_, key) => (
-            <div { ...{ key } } className="bg-gray-800 rounded-lg">
-                <img
-                    alt="..."
-                    src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
-                    className="w-full rounded-t-lg"
-                />
-                <div className="p-4">
-                    <p className="text-lg text-white font-bold font-nunito">Zwallet Web</p>
-                    <p className="text-md text-white font-semibold font-nunito">ZWallet adalah aplikasi yang bertujuan untuk mempermudah penyimpanan uang secara virtual, dibek...</p>
-                    <motion.button
+        portofolios.map((item, index) => (
+            <div key={index} className="bg-gray-800 rounded-lg p-4">
+                <img src={item.icon} width="60px" height="60px" />
+                <div className="mt-5">
+                    <p className="text-lg text-white font-bold font-nunito">{item.name}</p>
+                    <p className="text-md text-gray-200 font-semibold font-nunito">{item.description.length > 200 ? `${item.description.slice(0, 200)}...` : item.description}</p>
+                    <motion.a
                         whileHover={{ scale: 1.1 }}
-                        className="flex gap-2 mt-5 shadow-md rounded-xl px-4 py-2 bg-blue-500 items-center"
+                        target="_blank"
+                        href={item.url}
                     >
-                        <img src="/search.svg" width="18px" height="18px" />
-                        <p className="text-md text-white font-bold font-nunito">Lihat</p>
-                    </motion.button>
+                        <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            className="flex gap-2 mt-5 shadow-md rounded-xl px-4 py-2 bg-blue-500 items-center focus:outline-none"
+                        >
+                            <img src="/search.svg" width="18px" height="18px" />
+                            <p className="text-md text-white font-bold font-nunito">Lihat</p>
+                        </motion.button>
+                    </motion.a>
                 </div>
             </div>
         ))
@@ -43,29 +78,37 @@ const Home = () => {
                         <div className="flex justify-center mt-5">
                             <a
                                 className="text-3xl m-1 p-1 text-gray-200 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-gray-500 hover:text-white"
+                                target="_blank"
+                                href="https://github.com/qytela"
                             >
                                 <FaGithub />
                             </a>
                             <a
-                                className="text-3xl m-1 p-1 text-pink-600 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-pink-600 hover:text-white"
+                                className="text-3xl m-1 p-1 text-blue-500 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-blue-500 hover:text-white"
+                                target="_blank"
+                                href="https://www.linkedin.com/in/muhammad-julfansha"
                             >
-                                <FaDribbble />
+                                <FaLinkedinIn />
+                            </a>
+                            <a
+                                className="text-3xl m-1 p-1 text-red-500 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-red-500 hover:text-white"
+                                target="_blank"
+                                href="mailto: julfanshahidayah@gmail.com"
+                            >
+                                <FaRegEnvelope />
                             </a>
                             <a
                                 className="text-3xl m-1 p-1 text-blue-500 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-blue-500 hover:text-white"
+                                target="_blank"
+                                href="https://t.me/callmeFansa"
                             >
-                                <FaTwitter />
-                            </a>
-                            <a
-                                className="text-3xl m-1 p-1 text-green-500 rounded-full transition-colors duration-300 lg:m-2 lg:p-2 hover:bg-green-500 hover:text-white"
-                            >
-                                <FaRegEnvelope />
+                                <FaTelegramPlane />
                             </a>
                         </div>
                         <div className="w-56 mx-auto mt-5">
                             <motion.button
                                 whileHover={{ scale: 1.1 }}
-                                className="py-4 container rounded-3xl text-white font-bold font-poppins button"
+                                className="py-4 container rounded-3xl text-white font-bold font-poppins button focus:outline-none"
                             >
                                 Download CV
                             </motion.button>
@@ -90,14 +133,9 @@ const Home = () => {
                 </motion.div>
 
                 <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={{
-                        hidden: { x: 500, opacity: 0 },
-                        visible: { x: 0, opacity: 1 }
-                    }}
+                    animate={{ opacity: 1 }}
                     transition={{ duration: 1 }}
-                    className="container mx-auto pt-20"
+                    className="opacity-0 container mx-auto pt-20"
                 >
                     <p className="text-2xl font-bold text-white font-nunito uppercase title">Gallery</p>
                     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
@@ -163,18 +201,21 @@ const Home = () => {
                             <p className="text-2xl text-white font-bold font-poppins lg:text-3xl">Sedang mencari Pekerja atau Freelancer?</p>
                             <p className="text-md text-white font-poppins mt-8">Jika kamu tertarik ingin mempekerjakan atau bekerja sama dengan saya, kamu bisa menghubungi saya dibawah ini.</p>
                             <div className="flex items-center gap-4 mt-5">
-                                <motion.button
+                                <motion.a
                                     whileHover={{ rotate: 360, scale: 1.1 }}
                                     className="p-4 bg-white rounded-full"
+                                    href="tel:+62895336835534"
                                 >
                                     <img src="/call.svg" width="20px" height="20px" />
-                                </motion.button>
-                                <motion.button
+                                </motion.a>
+                                <motion.a
                                     whileHover={{ rotate: 360, scale: 1.1 }}
                                     className="p-4 bg-white rounded-full"
+                                    target="_blank"
+                                    href="https://www.linkedin.com/in/muhammad-julfansha"
                                 >
                                     <img src="/linkedin.svg" width="20px" height="20px" />
-                                </motion.button>
+                                </motion.a>
                             </div>
                             <p className="text-md text-white font-poppins mt-5">Designed by Fansa.</p>
                             <p className="text-md text-white font-poppins">Illustration made by IRA Design.</p>
